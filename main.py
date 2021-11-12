87695583a0dea189efedd17c77d88f205cda6663
Dan Mooney <daneemooney@gmail.com>
2021-11-12 10:38:35 -0700

    NPCs up and rolling. Commiting before I fuck it up

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
+
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    # player_x = int(screen_width / 2)
-    # player_y = int(screen_height / 2)
-
     tileset = tcod.tileset.load_tilesheet(
-            "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
-            )
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
 
     event_handler = EventHandler()
 
@@ -22,32 +20,20 @@ def main() -> None:
     npc = Entity(int(screen_width / 1 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     with tcod.context.new_terminal(
-            screen_width, 
-            screen_height,
-            tileset=tileset,
-            title="Roguelike",
-            vsync=True,
-            ) as context:
-                root_console = tcod.Console(screen_width, screen_height, order="F")
-                while True:
-                    root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
-
-                    context.present(root_console)
-
-                    root_console.clear()
-
-                    for event in tcod.event.wait():
-                        action = event_handler.dispatch(event)
-
-                        if action is None:
-                            continue
-
-                        if isinstance(action, MovementAction):
-                            player.move(dx=action.dx, dy=action.dy)
-
-                        if isinstance(action, EscapeAction):
-                            raise SystemExit()
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Roguelike",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
